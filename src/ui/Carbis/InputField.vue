46278a9800b2f7d8d4aa156fb7e14a1667c6018c
KiryaDtlv <dyatlov.kiryuha@gmail.com>
2023-05-15 18:27:32 +0700

    Добавление dense и outlined для поля ввода

--- src/ui/Carbis/InputField.vue
+++ src/ui/Carbis/InputField.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-text-field v-bind="$attrs" v-on="$listeners">
+  <v-text-field dense outlined v-bind="$attrs" v-on="$listeners">
     <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
   </v-text-field>
 </template>
